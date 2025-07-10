6cf0ffb29bdb7db9440ded00d94134a011f5035e
eugene-pande <esp.pandev@gmail.com>
2025-07-10 13:08:29 +1000

    Tailwind CSS set up

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>{{config('app.name', 'Laravel')}}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
@@ -124,7 +124,7 @@
                     </div>
 
                     <div class="text-center text-sm text-gray-500 dark:text-gray-400 sm:text-right sm:ml-0">
-                        Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
+                        {{config('app.name', 'Laravel')}}
                     </div>
                 </div>
             </div>
